0b632e091dbed22863a1d3fa5beb8ade8fc175dd
Trinh-Duc-Tai <trinhductai6869@gmail.com>
2021-02-25 21:56:31 +0700

    up all

---
+++ index.js
@@ -0,0 +1,162 @@
+// alert("Bài làm của Trịnh Đức Tài")
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn0 = document.querySelector("#playVideo");
+
+// Get the <span> element that closes the modal
+var close0 = document.getElementsByClassName("close")[0];
+
+//get the iframe from  youtube
+let playVid = document.querySelector("#video")
+
+btn0.onclick = function () {
+	modal.style.display = "block";
+};
+
+  // When the user clicks on <span> (x), close the modal
+close0.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  
+
+
+  // --------------bat dau modal 1-----------------------
+var close1 = document.getElementsByClassName("close1")[0];
+
+var modal1 = document.getElementById("myModal-1");
+
+// Get the button that opens the modal
+var btn1 = document.querySelector("#modal-1");
+
+
+btn1.onclick = function() {
+    modal1.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+close1.onclick = function() {
+    modal1.style.display = "none";
+  }
+
+// ------------ het modal 1 --------------------------------
+
+
+// --------------bat dau modal 2-----------------------
+var close2 = document.getElementsByClassName("close2")[0];
+
+var modal2 = document.getElementById("myModal-2");
+
+// Get the button that opens the modal
+var btn2 = document.querySelector("#modal-2");
+
+
+btn2.onclick = function() {
+    modal2.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+close2.onclick = function() {
+    modal2.style.display = "none";
+  }
+
+// ------------ het modal 2 --------------------------------
+
+// --------------bat dau modal 3-----------------------
+var close3 = document.getElementsByClassName("close3")[0];
+
+var modal3 = document.getElementById("myModal-3");
+
+// Get the button that opens the modal
+var btn3 = document.querySelector("#modal-3");
+
+
+btn3.onclick = function() {
+    modal3.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+close3.onclick = function() {
+    modal3.style.display = "none";
+  }
+
+// ------------ het modal 3 --------------------------------
+
+// --------------bat dau modal 4-----------------------
+var close4 = document.getElementsByClassName("close4")[0];
+
+var modal4 = document.getElementById("myModal-4");
+
+// Get the button that opens the modal
+var btn4 = document.querySelector("#modal-4");
+
+
+btn4.onclick = function() {
+    modal4.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+close4.onclick = function() {
+    modal4.style.display = "none";
+  }
+
+// ------------ het modal 4 --------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+// bat dau modal 2 
+// var modal2 = document.getElementById("myModal-2");
+
+// // Get the button that opens the modal
+// var btn2 = document.querySelector("#modal-2");
+
+
+// btn2.onclick = function() {
+
+//   }
+
+// close2.onclick = function() {
+//     modal2.style.display = "none";
+//   }
+
+
+// //   -------------
+// var close3 = document.getElementsByClassName("close3")[0];
+
+
+// // Get the button that opens the modal
+// var btn3 = document.querySelector("#modal-3");
+
+
+
+
+// close3.onclick = function() {
+//     modal3.style.display = "none";
+//   }
+//   //   -------------
+// var close4 = document.getElementsByClassName("close4")[0];
+
+// var modal4 = document.getElementById("myModal-4");
+
+// // Get the button that opens the modal
+// var btn4 = document.querySelector("#modal-4");
+
+
+// btn4.onclick = function() {
+//     modal4.style.display = "block";
+//   }
+
+
+
+
+
---
+++ tra_loi_cau_hoi.md
@@ -0,0 +1,28 @@
+# Trả lời câu hỏi
+## Phần 1: Trắc nghiệm
+1. d
+2. b
+3. b
+4. c
+5. b
+6. d
+7. a
+8. a
+9. c
+10. c
+
+## Phần 2: Tự luận
+1. Sửa xong code
+2. Đánh giá:
+  Về phần giao diện:
+    - Các khối skill hơi di dít quá.
+    - Màu sắc nền bị rời rạc ko khớp nhau, ko thấy hài hòa, đã dùng màu gradient thì để cả, 
+      bên dưới có khối màu đặc nhìn xấu.
+    - Giao diện chưa responsive.
+  Về phần code:
+    - xuống dòng hơi bừa bãi nên code hơi khó đọc
+    - code cũng đã có chú thích nhưng nên rõ ràng hơn, chú thích cả phần bắt đầu và phần kết thúc
+      ví dụ:  <!-- het div box-5  -->  <!-- het div box-container  -->
+    - tài liệu ảnh để vào thư mục riêng dễ nhìn rồi ok
+3. Sửa lại giao diện tý
+
